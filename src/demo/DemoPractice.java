930640c4cc3b307265fb231d5fdcf8cf714b4b33
maboo <maboo@10.1.10.49>
2024-01-09 20:32:01 -0500

    initial commit

---
+++ src/demo/DemoPractice.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class DemoPractice {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("Mabood Wahab");
+
+	
+		int a = 2;
+		int b = 4;
+		System.out.println(a +b);
+	}
+
+}
